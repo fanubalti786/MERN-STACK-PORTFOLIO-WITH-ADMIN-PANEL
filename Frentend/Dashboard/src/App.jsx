753e1dc27f22e48cbdc7dd3b8dc4abe0b16680bb
fanubalti786 <fanubalti786@gmail.com>
2025-02-20 17:01:17 +0500

    update

--- Frentend/Dashboard/src/App.jsx
+++ Frentend/Dashboard/src/App.jsx
@@ -1,8 +1,9 @@
 
+
 export default function App() {
   return (
     <div>
-      <h1 className="border-2 border-black p-2">Dashboard</h1>
+      <h1 className="text-3xl font-bold border-2 border-black p-2">Dashboard</h1>
     </div>
   )
 }
